87a49a572f24da193852b4bb4db152c90cca97e1
vlasovms <vlasovmakar@gmail.com>
2024-02-13 18:08:51 +0300

    Second commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void sayHello() {
         System.out.println("Привет мир!");
+        System.out.println("Привет мир2!");
     }
 
     public static void main(String[] args) {

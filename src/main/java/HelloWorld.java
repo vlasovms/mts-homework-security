ea63405d1d5f05dcdd30593613be52241d047b14
vlasovms <vlasovmakar@gmail.com>
2024-02-13 17:50:53 +0300

    First commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void sayHello() {
+        System.out.println("Привет мир!");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
